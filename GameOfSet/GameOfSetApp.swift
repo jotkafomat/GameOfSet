2b3861c7ceffc0a4750deb6365d4421d1d8a7a1b
jotkafomat <k-j@xl.wp.pl>
2021-01-31 12:11:57 +0000

    Initial Commit

---
+++ GameOfSet/GameOfSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfSetApp.swift
+//  GameOfSet
+//
+//  Created by Krzysztof Jankowski on 31/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct GameOfSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
